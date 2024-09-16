976a5a0a713943fe46c3597212f8eb3cadb7e383
HairyPotter <shriharikukade@gmail.com>
2024-09-16 22:21:58 +0530

    Both the sign-up buttons were made actionable, i.e, when the first button is clicked then the heading text changes, and the second button changes the lyrics of the Pink Floyd song to a Beatles song.

---
+++ index.js
@@ -0,0 +1,12 @@
+function signup(){
+    document.getElementById("wb").innerHTML = "Javascript makes this website even more awesome";
+}
+
+function testing(){
+    document.getElementById("news").innerHTML = `Try to realize it's all within yourself
+No one else can make you change
+And to see you're really only very small
+And life flows on within you and without you`;
+
+    document.getElementById("sou").innerHTML = "Within You, Without You - The Beatles"
+}
